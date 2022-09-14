c0ccb466678cbfce38a10caba3cfcd3074fc6afc
kodnennsy <lkvlkvlkv2003@yandex.ru>
2022-09-15 00:06:44 +0300

    Inint

--- src/ru/mirea/lab1/punkt5.java
+++ src/ru/mirea/lab1/punkt5.java
@@ -1,17 +1,15 @@
 package ru.mirea.lab1;
+
 import java.util.Scanner;
-public class Example {
+public class punkt5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int array[];
         int n = sc.nextInt();
         array = new int[n];
-        int a = 0;
         for(int i = 0; i < n; i++){
             array[i] = sc.nextInt();
-            a+=array[i];
+            System.out.println(array[i]);
         }
-        System.out.println(a);
-        System.out.println(a/n);
     }
 }
